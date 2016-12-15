8c98b51976d3c83838f6690cfc6f4cc37c2a6a88
Karson Chilcott <kjc38@aiken.cs.calvin.edu>
2016-12-14 22:30:17 -0500

    lab12

---
+++ lab12/server.js
@@ -0,0 +1,134 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only. Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+
+//var APP_PATH = path.join(__dirname, 'dist');
+
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var mongoVar = 'mongodb://admin:bjarne@ds135577.mlab.com:35577/cs336';
+
+MongoClient.connect(mongoVar, function (err, dbConnection){
+	if (err) throw err;
+	
+	db = dbConnection;
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+/*app.get('/api/comments', function(req, res) {
+
+	var collection = db.collection('lab10');
+
+	collection.find({}).toArray(function(err, data) {
+		assert.equal(err, null);
+		//console.log("Works?");
+		//console.log(data);
+		res.json(data);
+	});
+});
+
+app.post('/api/comments', function(req, res) {
+
+	var newComment = {
+		_id: Date.now(),
+		author: req.body.author,
+		text: req.body.text,
+	};
+
+	var collection = db.collection('lab10');
+
+	collection.insert(newComment, function(err, data){
+		assert.equal(err, null);
+		
+		res.json(data);
+	});
+});*/
+
+app.get('/api/comments/:id', function(req, res) {
+    db.collection("comments").find({"id": Number(req.params.id)}).toArray(function(err, docs) {
+        if (err) throw err;
+        res.json(docs);
+    });
+});
+
+app.put('/api/comments/:id', function(req, res) {
+    var updateId = Number(req.params.id);
+    var update = req.body;
+    db.collection('comments').updateOne(
+        { id: updateId },
+        { $set: update },
+        function(err, result) {
+            if (err) throw err;
+            db.collection("comments").find({}).toArray(function(err, docs) {
+                if (err) throw err;
+                res.json(docs);
+            });
+        });
+});
+
+app.post('/api/comments/:id', function(req, res) {
+    var updateId = Number(req.params.id);
+    var update = req.body;
+    db.collection('comments').updateOne(
+        { id: updateId },
+        { $set: update },
+        function(err, result) {
+            if (err) throw err;
+            db.collection("comments").find({}).toArray(function(err, docs) {
+                if (err) throw err;
+                res.json(docs);
+            });
+        });
+});
+
+app.delete('/api/comments/:id', function(req, res) {
+    db.collection("comments").deleteOne(
+        {'id': Number(req.params.id)},
+        function(err, result) {
+            if (err) throw err;
+            db.collection("comments").find({}).toArray(function(err, docs) {
+                if (err) throw err;
+                res.json(docs);
+            });
+        });
+});
+
+app.use('*', express.static(path.join(__dirname, 'dist')));
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
+
+});
